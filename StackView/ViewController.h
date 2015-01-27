537fd8fe185ab3f91becc96aac8f36320dc24d6b
Jay <jaygajjar77@gmail.com>
2015-01-27 17:43:16 +0530

    Initial Commit

---
+++ StackView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StackView
+//
+//  Created by Jay on 27/01/15.
+//  Copyright (c) 2015 UltraSonic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ StackView/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  StackView
+//
+//  Created by Jay on 27/01/15.
+//  Copyright (c) 2015 UltraSonic. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
