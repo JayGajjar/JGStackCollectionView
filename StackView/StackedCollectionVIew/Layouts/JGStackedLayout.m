2d69ffd9290dcee54c37099cbc7550c4f23fddb3
Jay <jaygajjar77@gmail.com>
2015-01-28 10:39:25 +0530

    First commit

--- StackView/StackedCollectionVIew/Layouts/JGStackedLayout.m
+++ StackView/StackedCollectionVIew/Layouts/JGStackedLayout.m
@@ -1,7 +1,7 @@
 //
 //  JGStackLayout.m
 //
-//  Created by Jay Gajjar on 07.01.15.
+//  Created by Jay on 27/01/15.
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
